d5c0be45b4f28532b49db467b603355e68261a20
15296728555 <15296728555@163.com>
2020-10-21 15:03:32 +0800

    666666666666666

--- gitTest/Program.cs
+++ gitTest/Program.cs
@@ -9,9 +9,7 @@ namespace gitTest
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!11111111111111111111");
             Console.WriteLine("Hello World!22222222222222222222222");
-            Console.WriteLine("Hello World!3333333333333333333333333");
-            Console.WriteLine("Hello World!44444444444444444");
-            Console.WriteLine("Hello World!555555555555555");
+            Console.WriteLine("Hello World!6666666666");
 
             Console.ReadLine();
         }
